4c22be321b90f57bfb935f9e111a4460ead6b626
Rodrigo Souza <rodrigocarlos1883@gmail.com>
2018-09-26 19:26:59 -0300

    atualizacao

--- src/main/java/com/autenticacao/apirest/resources/AutenticacaoResource.java
+++ src/main/java/com/autenticacao/apirest/resources/AutenticacaoResource.java
@@ -3,6 +3,7 @@ package com.autenticacao.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import com.autenticacao.apirest.models.Autenticacao;
 
 @RestController
 @RequestMapping(value="/api")
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AutenticacaoResource {
 	
 	@Autowired
