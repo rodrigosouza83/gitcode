fc419801b6773d634dbda157fae112da118c05ab
Rodrigo Souza <rodrigocarlos1883@gmail.com>
2018-09-28 11:27:53 -0300

    code updated

--- src/main/java/com/autenticacao/apirest/models/Autenticacao.java
+++ src/main/java/com/autenticacao/apirest/models/Autenticacao.java
@@ -17,14 +17,6 @@ public class Autenticacao implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
 	}
